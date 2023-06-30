354eafe9ec86e28d267d38afb7194edc0b1f66ef
Pablo <pavloglez@gmail.com>
2023-06-30 11:51:20 -0600

    Fix package name error

--- app/src/main/java/glez/pavlo/showcase/MainActivity.kt
+++ app/src/main/java/glez/pavlo/showcase/MainActivity.kt
@@ -1,4 +1,4 @@
-package glez.pavloshowcase
+package glez.pavlo.showcase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,7 @@ import glez.pavloshowcase.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
